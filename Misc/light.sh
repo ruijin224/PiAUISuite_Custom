37c5079764a2fdb896efceb3328af8954c6dae3b
Rui Jin <rjin@mit.edu>
2013-09-22 16:56:42 -0400

    Changed default settings

--- Misc/light.sh
+++ Misc/light.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-aplay -c 1 -t wav -r 44100 -f cd clap.wav
+aplay -c 1 -t wav -r 44100 -f cd /home/pi/PiAUISuite_Custom/Misc/clap.wav
 sleep 0.5
-aplay -c 1 -t wav -r 44100 -f cd clap.wav
+aplay -c 1 -t wav -r 44100 -f cd /home/pi/PiAUISuite_Custom/Misc/clap.wav
 sleep 0.5
-aplay -c 1 -t wav -r 44100 -f cd clap.wav
+aplay -c 1 -t wav -r 44100 -f cd /home/pi/PiAUISuite_Custom/Misc/clap.wav
 
