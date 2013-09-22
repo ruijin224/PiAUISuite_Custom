fd0102f4373ddb9c2790c92fad004ae22106a6d0
Rui Jin <rjin@mit.edu>
2013-09-22 17:02:47 -0400

    Changed clap interval

--- Misc/light.sh
+++ Misc/light.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aplay -c 1 -t wav -r 44100 -f cd /home/pi/PiAUISuite_Custom/Misc/clap.wav
-sleep 0.5
+sleep 1
 aplay -c 1 -t wav -r 44100 -f cd /home/pi/PiAUISuite_Custom/Misc/clap.wav
 sleep 0.2
 aplay -c 1 -t wav -r 44100 -f cd /home/pi/PiAUISuite_Custom/Misc/clap.wav
